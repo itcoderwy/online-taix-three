2e0f8635ffc4fd66007ef9d231201d7f68f655c2
itcarswy <itsuperwww@163.com>
2021-04-27 16:38:42 +0800

    api-passenger--->优化eureka

---
+++ api-passenger/src/test/java/com/cars/apipassenger/ApiPassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars.apipassenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPassengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
